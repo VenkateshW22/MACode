9bd126b42c3de1b78b67cbb7a3f3d64f8b59d5b9
VenkateshW22 <venkateshwork2212@gmail.com>
2025-08-26 10:06:54 +0530

    Day 21 code model

---
+++ D21/monolith/src/main/java/com/ecommerce/monolith/model/Order.java
@@ -0,0 +1,20 @@
+package com.ecommerce.monolith.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Long productId;
+    private int quantity;
+    private LocalDateTime orderDate;
+    private String status;
+}
