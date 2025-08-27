8d7a0c874620112bd34b367ed460aee2563da648
VenkateshW22 <venkateshwork2212@gmail.com>
2025-08-27 12:49:18 +0530

    Day 22 Code

--- D21/monolith/src/main/java/com/ecommerce/monolith/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.monolith.repository;
-
-import com.ecommerce.monolith.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
